b5c526a8f829cdd5be78f61b8b63f861fe44d49d
Arturo Bruno Carrillo <lord.the.gatekeeper@gmail.com>
2022-07-21 16:45:13 -0500

    Start processing CSV file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
 import json
+import logging
 import urllib.parse
+from pathlib import Path
 
 import boto3
 
@@ -9,16 +12,23 @@ s3 = boto3.client('s3')
 
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
+    logging.info(f'Received event: {json.dumps(event, indent=2)}')
 
-    # Get the object from the event and show its content type
+    filename = event['Records'][0]['s3']['object']['key']
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    key = urllib.parse.unquote_plus(filename, encoding='utf-8')
 
     try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
-        return response['ContentType']
+        s3_object = s3.Object(bucket, key)
+        csv_data = s3_object.get()['Body'].read().decode('utf-8').splitlines()
     except Exception as e:
         print(f'Error getting object {key} from bucket {bucket}: "{e}"')
         raise e
+
+    client_transactions = csv.DictReader(csv_data)
+    client_name = Path(filename).stem.replace('_', ' ')
+    print(f'Client: "{client_name}"')
+
+    for transaction in client_transactions:
+        print(transaction)
+        print()
