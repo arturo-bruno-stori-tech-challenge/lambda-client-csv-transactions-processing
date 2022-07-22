fb2a993f73232b5b223f796d6fa0649ce993bd27
Arturo Bruno Carrillo <lord.the.gatekeeper@gmail.com>
2022-07-21 20:31:15 -0500

    Add some debug code

--- main.py
+++ main.py
@@ -51,15 +51,19 @@ def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(filename, encoding='utf-8')
 
+    client_name = Path(filename).stem.replace('_', ' ')
+    print(f'Client: "{client_name}"')
+    print(f'Bucket: "{bucket}"')
+    print(f'Key: "{key}"')
+
     try:
-        csv_file = s3.get_object(Bucket=bucket, Key=key)['Body'].read()
+        csv_file = s3.get_object(Bucket=bucket, Key=key)['Body'].read().splitlines()
+        print('csv_file', csv_file)
     except Exception as e:
         print(f'Error getting object {key} from bucket {bucket}: "{e}"')
         raise e
 
     client_transactions = csv.DictReader(csv_file)
-    client_name = Path(filename).stem.replace('_', ' ')
-    print(f'Client: "{client_name}"')
 
     # Move CSV file to "processing" folder
     print('key', key)
@@ -73,3 +77,48 @@ def lambda_handler(event, context):
     # Move the CSV file to "processed" folder
 
     # Send event to trigger
+
+
+
+Received event: {
+    "Records": [
+        {
+            "eventVersion": "2.1",
+            "eventSource": "aws:s3",
+            "awsRegion": "us-east-2",
+            "eventTime": "2022-07-22T01:22:03.522Z",
+            "eventName": "ObjectCreated:Put",
+            "userIdentity": {
+                "principalId": "A1T8W4UQT29LXO"
+            },
+            "requestParameters": {
+                "sourceIPAddress": "189.203.6.28"
+            },
+            "responseElements": {
+                "x-amz-request-id": "1ZSF00B7VA4K4ZCN",
+                "x-amz-id-2": "/8PtAqbaorEEe4A8YKw4n4rAJWi4FbidDeqodOy/CX44Js+eYR3kvgwEt0hTMMLHlClE1I1kVvdTvfXzCOTme6lhnR/MYLN+"
+            },
+            "s3": {
+                "s3SchemaVersion": "1.0",
+                "configurationId": "71419a86-41a0-4453-8f8a-cb69875a9ae8",
+                "bucket": {
+                    "name": "clients-transactions",
+                    "ownerIdentity": {
+                        "principalId": "A1T8W4UQT29LXO"
+                    },
+                    "arn": "arn:aws:s3:::clients-transactions"
+                },
+                "object": {
+                    "key": "unprocessed/Todd_Long.csv",
+                    "size": 181324,
+                    "eTag": "5b895878b974402cc94f96b10ac570e6",
+                    "versionId": "Os_4gEYXcOiG4jKxwE6UIg5dXqGaFLIE",
+                    "sequencer": "0062D9FBBB721E0C50"
+                }
+            }
+        }
+    ]
+}
+
+host: "database-1-instance-1.csf7zau4mboa.us-east-2.rds.amazonaws.com"
+user: "summary"
